296690dda6ffc3186d3d2c1c36b220f504156c3c
Beezee_Boi <beezeeboi.znoobtube@gmail.com>
2022-12-15 04:31:18 -0500

    add error handling

--- app.js
+++ app.js
@@ -27,44 +27,70 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
-app.get('/products', async (req, res) => {
-  const products = await Product.find({});
-  res.render('products/index', { products });
+app.get('/products', async (req, res, next) => {
+  try {
+    const products = await Product.find({});
+    res.render('products/index', { products });
+  } catch (e) {
+    next(e);
+  }
 });
 
 app.get('/products/new', (req, res) =>{
   res.render('products/new', { productTypes });
 });
 
-app.get('/products/:id', async (req, res) => {
-  const { id } = req.params;
-  const product = await Product.findById(id);
-  res.render('products/detail', { product });
+app.get('/products/:id', async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    res.render('products/detail', { product });
+  } catch (e) {
+    next(e);
+  }
 });
 
-app.get('/products/:id/edit', async (req, res) => {
-  const { id } = req.params;
-  const product = await Product.findById(id);
-  res.render('products/edit', { product, productTypes });
+app.get('/products/:id/edit', async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    res.render('products/edit', { product, productTypes });
+  } catch (e) {
+    next(e);
+  }
 });
 
-app.post('/products/create', async (req, res) => {
-  const newProduct = new Product(req.body);
-  await newProduct.save();
-  res.redirect(`/products/${newProduct._id}`);
+app.post('/products/create', async (req, res, next) => {
+  try {
+    const newProduct = new Product(req.body);
+    await newProduct.save();
+    res.redirect(`/products/${ newProduct._id }`);
+  } catch (e) {
+    next(e);
+  }
 });
 
-app.put('/products/:id', async (req, res) => {
-  const { id } = req.params;
-  const editedProduct = await Product.findByIdAndUpdate(id, req.body, { runValidators: true });
-  res.redirect(`/products/${editedProduct._id}`);
+app.put('/products/:id', async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const editedProduct = await Product.findByIdAndUpdate(id, req.body, { runValidators: true });
+    res.redirect(`/products/${ editedProduct._id }`);
+  } catch (e) {
+    next(e);
+  }
 });
 
-app.delete('/products/:id', async (req, res) => {
-  const { id } = req.params;
-  await Product.findByIdAndDelete(id);
-  res.redirect('/products');
-})
+app.delete('/products/:id', async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    await Product.findByIdAndDelete(id);
+    res.redirect('/products');
+  } catch (e) {
+    next(e);
+  }
+});
+
+
 
 app.listen(port, () => {
   console.log(`Server running on port: ${ port }`);
